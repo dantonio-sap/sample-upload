3c729a058a3a04665e6546e451577e7da50884be
Dan Antonio <dan.antonio@sap.com>
2021-10-19 20:31:34 +0000

    fixes

--- app/upload-ui/dist/controller/View1-dbg.controller.js
+++ app/upload-ui/dist/controller/View1-dbg.controller.js
@@ -1,92 +1,105 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-],
-	/**
-	 * @param {typeof sap.ui.core.mvc.Controller} Controller
-	 */
-	function (Controller) {
-		"use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller"],
+  /**
+   * @param {typeof sap.ui.core.mvc.Controller} Controller
+   */
+  function (Controller) {
+    "use strict";
 
-		return Controller.extend("uploadui.controller.View1", {
-            apiUrl: 'api/catalog/',
+    return Controller.extend("uploadui.controller.View1", {
+      apiUrl: "api/catalog/",
 
-			onInit: function () {
+      onInit: function () {},
+      onAfterItemAdded: function (oEvent) {
+        var item = oEvent.getParameter("item");
+        this._createEntity(item)
+          .then((id) => {
+            this._uploadContent(item, id);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      },
 
-            },
-            onAfterItemAdded: function (oEvent) {
-				var item = oEvent.getParameter("item")
-				this._createEntity(item)
-				.then((id) => {
-					this._uploadContent(item, id);
-				})
-				.catch((err) => {
-					console.log(err);
-				})
-			},
+      onUploadCompleted: function () {
+        var oUploadSet = this.byId("uploadSet");
+        oUploadSet.removeAllIncompleteItems();
+        oUploadSet.getBinding("items").refresh();
+      },
 
-			onUploadCompleted: function () {
-				var oUploadSet = this.byId("uploadSet");
-				oUploadSet.removeAllIncompleteItems();
-				oUploadSet.getBinding("items").refresh();
-			},
+      _createEntity: function (item) {
+        var data = {
+          mediaType: item.getMediaType(),
+          fileName: item.getFileName(),
+          size: item.getFileObject().size,
+        };
 
-			_createEntity: function (item) {
-					var data = {
-						mediaType: item.getMediaType(),
-						fileName: item.getFileName(),
-						size: item.getFileObject().size
-					};
-	
-					var settings = {
-						url: `${this.apiUrl}/Attachments`,
-						method: "POST",
-						headers: {
-							"Content-type": "application/json"
-						},
-						data: JSON.stringify(data)
-					}
-	
-				return new Promise((resolve, reject) => {
-					$.ajax(settings)
-						.done((results, textStatus, request) => {
-							resolve(results.ID);
-						})
-						.fail((err) => {
-							reject(err);
-						})
-				})				
-			},
+        return new Promise((resolve, reject) => {
+          var oBinding = this.getView().getModel().bindList("/Attachments");
+          oBinding.attachCreateCompleted(
+            function (oEvent) {
+              if (!oEvent.getParameter("success")) {
+                reject();
+              } else {
+                resolve(oEvent.getParameter("context").getPath());
+              }
+            }.bind(this)
+          );
+          oBinding.create(data);
+        });
 
-			_uploadContent: function (item, id) {
-				var url = `${this.apiUrl}/Attachments(${id})/content`
-				item.setUploadUrl(url);	
-				var oUploadSet = this.byId("uploadSet");
-				oUploadSet.setHttpRequestMethod("PUT")
-				oUploadSet.uploadItem(item);
-            },
-            
-            formatThumbnailUrl: function (mediaType) {
-				var iconUrl;
-				switch (mediaType) {
-					case "image/png":
-						iconUrl = "sap-icon://card";
-						break;
-					case "text/plain":
-						iconUrl = "sap-icon://document-text";
-						break;
-					case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
-						iconUrl = "sap-icon://excel-attachment";
-						break;
-					case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
-						iconUrl = "sap-icon://doc-attachment";
-						break;
-					case "application/pdf":
-						iconUrl = "sap-icon://pdf-attachment";
-						break;
-					default:
-						iconUrl = "sap-icon://attachment";
-				}
-				return iconUrl;
-			}
-		});
-	});
+        // var settings = {
+        //   url: `${this.apiUrl}/Attachments`,
+        //   method: "POST",
+        //   headers: {
+        //     "Content-type": "application/json",
+        //   },
+        //   data: JSON.stringify(data),
+        // };
+
+        // return new Promise((resolve, reject) => {
+        //   $.ajax(settings)
+        //     .done((results, textStatus, request) => {
+        //       resolve(results.ID);
+        //     })
+        //     .fail((err) => {
+        //       reject(err);
+        //     });
+        // });
+      },
+
+      _uploadContent: function (item, id) {
+        var serviceUrl = this.getView().getModel().sServiceUrl;
+        var url = `${serviceUrl}${id}/content`;
+        item.setUploadUrl(url);
+        var oUploadSet = this.byId("uploadSet");
+        oUploadSet.setHttpRequestMethod("PUT");
+        oUploadSet.uploadItem(item);
+      },
+
+      formatThumbnailUrl: function (mediaType) {
+        var iconUrl;
+        switch (mediaType) {
+          case "image/png":
+            iconUrl = "sap-icon://card";
+            break;
+          case "text/plain":
+            iconUrl = "sap-icon://document-text";
+            break;
+          case "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            iconUrl = "sap-icon://excel-attachment";
+            break;
+          case "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            iconUrl = "sap-icon://doc-attachment";
+            break;
+          case "application/pdf":
+            iconUrl = "sap-icon://pdf-attachment";
+            break;
+          default:
+            iconUrl = "sap-icon://attachment";
+        }
+        return iconUrl;
+      },
+    });
+  }
+);
